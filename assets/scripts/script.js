d37d7d8161834ea1657933f67e7e93b8772f5a17
malilaychris <malilaychris@gmail.com>
2019-11-01 02:12:32 -0500

    Add point deduction

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,12 +14,13 @@ let submitScore = document.getElementById('submitScore');
 
 let questionText = document.getElementById('questionText');
 let timerText = document.getElementById('timerText');
+let answerConfirm = document.getElementById('answerConfirm');
 let scoreText = document.getElementById('scoreText');
 
 defaultPage();
 
 let questionIndex = 0;
-let timeInitial = 50;
+let timeInitial = 60;
 let time;
 let finalScore;
 
@@ -29,9 +30,6 @@ function defaultPage() {
   resultsSection.style.display = 'none';
 
   startQuizButton.addEventListener('click', startQuiz);
-  for(let i = 0; i < 4; i++) {
-    answerButton[i].addEventListener('click', nextQuestion);
-  }
 }
 
 function startQuiz() {
@@ -46,7 +44,11 @@ function startQuiz() {
 
 function timer() {
   timeInitial--;
-  timerText.textContent = "Time: " + timeInitial;
+  timerText.textContent = 'Time: ' + timeInitial;
+
+  if(timeInitial === 0) {
+    clearInterval(time);
+  }
 }
 
 function showQuestion() {
@@ -54,10 +56,20 @@ function showQuestion() {
   for(let i = 0; i < questions[questionIndex].choices.length; i++) {
     answerButton[i].textContent = questions[questionIndex].choices[i];
   }
+  for(let i = 0; i < 4; i++) {
+    answerButton[i].addEventListener('click', nextQuestion);
+  }
 }
 
 function nextQuestion() {
-  if(questionIndex < questions.length - 1) {
+  if(this.id == 'answerButton' + questions[questionIndex].answer) {
+    answerConfirm.textContent = 'Correct!';
+  } else {
+    answerConfirm.textContent = 'Wrong';
+    timeInitial = timeInitial - 10;
+  }
+
+  if(questionIndex < questions.length - 1 && timeInitial > 0) {
     questionIndex++;
     showQuestion(questionIndex);
   } else {
@@ -70,12 +82,12 @@ function getResults() {
   resultsSection.style.display = 'block';
   finalScore = timeInitial;
 
-  scoreText.textContent = "Final Score: " + finalScore;
+  scoreText.textContent = 'Final Score: ' + finalScore;
 
   for (let i = 0; i < localStorage.length; i++) {
     let name = localStorage.key(i);
     let value = localStorage.getItem(name);
-    scoreList.innerHTML += "<div>" + name + " " + value + " </div>";
+    scoreList.innerHTML += '<div>' + name + ' ' + value + '</div>';
   }
 
   submitScore.addEventListener('click', storeScore);
@@ -85,11 +97,11 @@ function storeScore() {
   let name = nameInput.value;
   localStorage.setItem(name, finalScore);
 
-  scoreList.innerHTML = "";
+  scoreList.innerHTML = '';
 
   for (let i = 0; i < localStorage.length; i++) {
     let name = localStorage.key(i);
     let value = localStorage.getItem(name);
-    scoreList.innerHTML += "<div>" + name + " " + value + " </div>";
+    scoreList.innerHTML += '<div>' + name + ' ' + value + '</div>';
   }
 }
