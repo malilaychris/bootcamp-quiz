066f1d6045fd3641bfad594a3f3e27d656a9bc27
malilaychris <malilaychris@gmail.com>
2019-11-01 01:56:38 -0500

    Add high score and submit functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,16 @@
 let introductionSection = document.getElementById('introductionSection');
 let questionSection = document.getElementById('questionSection');
 let resultsSection = document.getElementById('resultsSection');
+let scoreList = document.getElementById('scoreList');
+
+let nameInput = document.getElementById('nameInput');
 
 let startQuizButton = document.getElementById('startQuizButton');
 let answerButton = [];
 for(let i = 0; i < 4; i++) {
   answerButton[i] = document.getElementById('answerButton' + i);
 }
+let submitScore = document.getElementById('submitScore');
 
 let questionText = document.getElementById('questionText');
 let timerText = document.getElementById('timerText');
@@ -17,6 +21,7 @@ defaultPage();
 let questionIndex = 0;
 let timeInitial = 50;
 let time;
+let finalScore;
 
 function defaultPage() {
   introductionSection.style.display = 'block';
@@ -63,6 +68,28 @@ function nextQuestion() {
 function getResults() {
   questionSection.style.display = 'none';
   resultsSection.style.display = 'block';
+  finalScore = timeInitial;
 
-  scoreText.textContent = "Final Score: " + timeInitial;
+  scoreText.textContent = "Final Score: " + finalScore;
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let name = localStorage.key(i);
+    let value = localStorage.getItem(name);
+    scoreList.innerHTML += "<div>" + name + " " + value + " </div>";
+  }
+
+  submitScore.addEventListener('click', storeScore);
+}
+
+function storeScore() {
+  let name = nameInput.value;
+  localStorage.setItem(name, finalScore);
+
+  scoreList.innerHTML = "";
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let name = localStorage.key(i);
+    let value = localStorage.getItem(name);
+    scoreList.innerHTML += "<div>" + name + " " + value + " </div>";
+  }
 }
